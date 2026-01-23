9c4bd70c643a8658a36bde322feb1577b16e5cb5
Uwe Balzer <uwe.balzer@bauerxcel.de>
2026-01-23 17:34:07 +0100

    updater argocd

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def version():
-    return "This is image version 1.0.111"
+    return "This is image version 1.0.1"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
