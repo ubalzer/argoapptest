ececeeeafe0e8f8b857b0a0a32d654bf32f9a728
Uwe Balzer <uwe.balzer@bauerxcel.de>
2026-01-23 17:47:11 +0100

    hmm

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def version():
-    return "This is image version 1.0.1"
+    return "This is image version 1.0.dreckmist"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
